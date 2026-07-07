842e84c8f23f50153aec9a4e7fdb8e459a64c55e
sftdst <bambadiaw93@gmail.com>
2026-07-07 08:22:10 +0000

    fix: supprimer TrustHosts localhost pour production

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustHosts(at: ['localhost']);
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
